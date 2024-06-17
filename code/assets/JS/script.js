7b90c573bb2ee15a35e1dba54a56ee1c2f0ae89a
Falk1n <113801587+falkones04@users.noreply.github.com>
2024-06-17 05:14:53 -0300

    e

--- code/assets/JS/script.js
+++ code/assets/JS/script.js
@@ -28,30 +28,31 @@ document.addEventListener("DOMContentLoaded", function () {
         perfilDiv.className = "row";
 
         perfilDiv.innerHTML = `
-            <div class="col-12 col-md-3 d-flex align-content-center flex-wrap">
-                <img src="${dados.avatar_url}" width="240px" height="300px" class="mb-2">
+            <div class="col-12 col-md-3">
+                <img src="${dados.avatar_url}" class="img-fluid mb-2" alt="Perfil Image">
             </div>
             <div class="col-12 col-md-9">
                 <h3>${dados.name}</h3>
                 <p>${dados.bio}</p>
-                <b>Localização:</b><p class="mb-1" style="display: inline-block"> ${dados.location}</p><br>
-                <b>Site:</b><p class="mt-2" style="display: inline-block"> ${dados.blog}</p>
-                <span class="float-end">
-                    <img class="icon pb-2" src="./assets/Imagens/follow.png">
-                    <span class="text-body-dark text-dark fw-bold fs-5 d-inline-block text-center">${dados.followers}</span>
-                </span>
+                <b>Localização:</b> <p style="display: inline-block">${dados.location}</p><br>
+                <b>Site:</b> <p style="display: inline-block">${dados.blog}</p>
+                <div class="mt-3">
+                    <img src="./assets/Imagens/follow.png" class="icon"> <span class="fw-bold">${dados.followers}</span>
+                </div>
 
-                <div class="row">
-                    <div class="col-12 col-md-4 pt-md-4 m-0" style="padding-left: 0px; padding-right: 0px;">
-                        <a href="https://www.instagram.com/falkone_0405/" target="_blank"><img src="./assets/Imagens/instagram.png" alt="..." class="icon mx-1"></a>
-                        <a href="https://www.linkedin.com/in/gabriel-falk-160229182/" target="_blank"><img src="./assets/Imagens/linkedin.png" alt="..." class="icon mx-1"></a>
-                        <a href="mailto:gabrielmenezes0405@gmail.com" target="_blank"><img src="./assets/Imagens/gmail.png" alt="..." class="icon mx-1"></a>
-                        <a href="https://x.com/falkyu" target="_blank"><img src="./assets/Imagens/x.png" alt="..." class="icon mx-1"></a>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <a href="https://www.instagram.com/falkone_0405/" target="_blank"><img src="./assets/Imagens/instagram.png" class="icon mx-1"></a>
+                        <a href="https://www.linkedin.com/in/gabriel-falk-160229182/" target="_blank"><img src="./assets/Imagens/linkedin.png" class="icon mx-1"></a>
+                        <a href="mailto:gabrielmenezes0405@gmail.com" target="_blank"><img src="./assets/Imagens/gmail.png" class="icon mx-1"></a>
+                        <a href="https://x.com/falkyu" target="_blank"><img src="./assets/Imagens/x.png" class="icon mx-1"></a>
                     </div>
                 </div>
             </div>
-            <h3>Repositórios (${dados.public_repos})</h3>
-            <hr>
+            <div class="col-12">
+                <h3>Repositórios (${dados.public_repos})</h3>
+                <hr>
+            </div>
         `;
 
         sessao1.appendChild(perfilDiv);
@@ -71,8 +72,8 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div class="card">
                         <div class="card-header fw-bold">${repo.name}</div>
                         <div class="card-body">
-                            <p>${repo.description}</p>
-                            <span class="badge bg-danger">${repo.language}</span>
+                            <p>${repo.description || 'No description'}</p>
+                            <span class="badge bg-danger p-2">${repo.language}</span>
                             <div class="float-end">
                                 <img src="./assets/Imagens/star.png" class="icon mx-2"><span>${repo.stargazers_count}</span>
                                 <img src="./assets/Imagens/view.png" class="icon mx-2"><span>${repo.watchers_count}</span>
@@ -100,8 +101,8 @@ document.addEventListener("DOMContentLoaded", function () {
             carouselIndicators += `<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${index}" class="${isActive}" aria-current="true" aria-label="Slide ${index + 1}"></button>`;
             carouselItems += `
                 <div class="carousel-item ${isActive}">
-                    <a href="${item.url_cont}"> 
-                        <img src="${item.url_img}" class="d-block w-100" alt="..." id="carrosel">
+                    <a href="${item.url_cont}">
+                        <img src="${item.url_img}" class="d-block w-100" id="carrosel" alt="Slide ${index + 1}">
                     </a>
                     <div class="carousel-caption d-none d-md-block">
                         <h5>${item.titulo}</h5>
@@ -134,26 +135,25 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function MostrarAmigos(amigos) {
-        const telaAmigos = document.getElementById("DataSession4"); 
+        const telaAmigos = document.getElementById("DataSession4");
         let linha = document.createElement("div");
         linha.className = "row";
+
         amigos.forEach(item => {
             let newDiv = document.createElement("div");
-            newDiv.className = "card col-12 col-sm-12 col-md-3 m-1";
+            newDiv.className = "col-12 col-sm-6 col-md-3 m-1";
             newDiv.innerHTML = `
-                <div class="card-body">
-                    <div class="col-12">
-                    <img src="${item.url_img}" class="divImgColegas">
-                    </div>
-                    <div class="card-header text-center"><a href="#" target="_blank"
-                        class="text-decoration-none text-dark">${item.nome}</a></div>
-                        <span class="badge bg-dark mt-4"><img src="./Assets/Imagens/git.png"class="icon">Github</span>
-                    </div>
+            <div class="card">
+                 <div class ="card-body">
+                    <img src="${item.url_img}" class="img-fluid divImgColegas">
+                    <div class="card-header text-center"><a href="#" target="_blank" class="text-decoration-none text-dark">${item.nome}</a></div>
+                    <button class="badge bg-dark mt-2 d-block"><img src="./assets/Imagens/git.png" class="icon"> Github</button>
+                </div>
             </div>
-        </div>
             `;
             linha.appendChild(newDiv);
         });
+
         telaAmigos.appendChild(linha);
     }
 });
